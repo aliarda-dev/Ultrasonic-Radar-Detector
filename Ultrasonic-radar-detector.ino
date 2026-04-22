5e190e99978a2ccc6cfec869ded8f3a7d0f791d9
Ali Arda <aliardakocaborek.contact@gmail.com>
2026-04-22 22:04:53 +0300

    Arduino IDE Code for Ultrasonic Radar Detector

--- Ultrasonic-radar-detector.ino
+++ Ultrasonic-radar-detector.ino
@@ -1,45 +1,64 @@
 //Ultrasonic Radar Detector Project
 //Ali Arda Kocabörek | Akdeniz EEE
-//Bu kodda henuz arduinoda yeni oldugumdan dolayi AI destegi kullanilmistir ancak mantik ve algoritma kismi bana aittir.
 
 #include <Servo.h>
-
-Servo radarServo; //Bu bizim motoruma verdigimiz adlandirma
-void checkDistance();
+#include <Arduino.h>
 
 const int trigPin = 9;
-const int echoPin = 10;
+const int echoPin = 10;  
 const int buzzerPin = 12;
 
-long duration; //sesin hava kalma suresi
-int distance; //sesin esyaya carpana kadar gidip gelme toplam suresi
 
-void setup() {
+Servo radarServo; //Bu bizim motoruma verdigimiz adlandirma
+
+void checkDistance();
+
+int main(void){
+	
+	init();
+    
   radarServo.attach(11);
+  
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
+  
+  
   Serial.begin(9600);
-}
-
-void loop() {
-  for(int i = 0; i <= 180; i++){  
+  
+  while(1)
+  {
+  	
+  	 for(int i = 0; i <= 180; i++){  
     radarServo.write(i);
     delay(30); 
+    
     checkDistance();
+    
   }
-  for(int i = 180; i >= 0; i--){  
+     for(int i = 180; i >= 0; i--){  
     radarServo.write(i);
     delay(30);
+    
     checkDistance();
   }
+}
+  	
+  	
 }
 
 void checkDistance() {
+	
+	  long duration; //sesin hava kalma suresi
+    int distance; //sesin esyaya carpana kadar gidip gelme toplam suresi 
+	
+	
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
+  
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
+  
   digitalWrite(trigPin, LOW);
   
   duration = pulseIn(echoPin, HIGH);
